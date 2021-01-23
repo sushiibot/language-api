fd303de210a9f2b357339e096c0501f763093d60
tzushimelon <4021761+tzushimelon@users.noreply.github.com>
2021-01-22 20:05:22 -0800

    feat: Add client, run server on default

--- src/client.rs
+++ src/client.rs
@@ -1,5 +1,7 @@
 use hello_world::greeter_client::GreeterClient;
 use hello_world::HelloRequest;
+use std::env;
+use tracing_subscriber::EnvFilter;
 
 pub mod hello_world {
     tonic::include_proto!("helloworld");
@@ -7,15 +9,28 @@ pub mod hello_world {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut client = GreeterClient::connect("http://[::1]:50051").await?;
+    dotenv::dotenv().ok();
+    tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::from_default_env())
+        .init();
+
+    let args: String = env::args().skip(1).collect::<Vec<_>>().join(" ");
+
+    let addr = env::var("SERVER_ADDR")
+        .unwrap_or_else(|_| "0.0.0.0:8080".to_string());
+
+    let mut client = GreeterClient::connect(addr).await?;
 
     let request = tonic::Request::new(HelloRequest {
-        name: "Tonic".into(),
+        name: args,
     });
+
+    tracing::info!("Request: {:#?}", request);
 
     let response = client.say_hello(request).await?;
 
-    println!("RESPONSE={:?}", response);
+    tracing::info!("Response: {:#?}", response);
+
 
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
+use lingua::Language::{English, French, German, Spanish, Turkish};
+use lingua::{Language, LanguageDetector, LanguageDetectorBuilder};
+use std::net::SocketAddr;
+use std::env;
 use tonic::{transport::Server, Request, Response, Status};
 use tracing_subscriber::EnvFilter;
-
-use lingua::Language::{English, French, German, Spanish};
-use lingua::{Language, LanguageDetector, LanguageDetectorBuilder};
 
 use hello_world::greeter_server::{Greeter, GreeterServer};
 use hello_world::{HelloReply, HelloRequest};
@@ -42,14 +43,21 @@ impl Greeter for MyGreeter {
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     dotenv::dotenv().ok();
+
     tracing_subscriber::fmt()
         .with_env_filter(EnvFilter::from_default_env())
         .init();
 
-    let addr = "[::1]:50051".parse().unwrap();
+    let addr: SocketAddr = env::var("LISTEN_ADDR")
+        .unwrap_or_else(|_| "0.0.0.0:8080".to_string())
+        .parse::<SocketAddr>()
+        .expect("Failed to parse LISTEN_ADDR as a SocketAddr");
 
-    let languages = vec![English, French, German, Spanish];
+    let languages = vec![English, French, German, Spanish, Turkish];
+
+    tracing::info!("Loading languages: {:?}", languages);
     let detector: LanguageDetector = LanguageDetectorBuilder::from_languages(&languages).build();
+    tracing::info!("Finished loading language detector");
 
     let greeter = MyGreeter::new(detector);
 
